28cefd556fe2fccbbb3ca8747a8b1a5cd4c7a8e7
rehgon <rehgon@5ce971c1-67d6-bedd-deea-c9cf13bb2f1f>
2010-05-12 18:13:32 +0000

    kartenorakel verbessert

--- trunk/src/com/google/code/kartenOrakel/KartenOrakloid.java
+++ trunk/src/com/google/code/kartenOrakel/KartenOrakloid.java
@@ -1,6 +1,7 @@
 package com.google.code.kartenOrakel;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,14 +18,14 @@ public class KartenOrakloid extends Activity {
 	private KartenOrakel orakel;
 	private String[] karte;
 	private ProgressBar progress;
-	
+
 	private boolean istNeuesSpiel;
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.kartenorakel);
-		
+
 		orakel = new KartenOrakel();
 		ausgabe = (TextView) findViewById(R.id.kartenOrakelAusgabe);
 		tieferButton = (Button) findViewById(R.id.kartenOrakeltieferButton);
@@ -34,10 +35,10 @@ public class KartenOrakloid extends Activity {
 		image = (ImageButton) findViewById(R.id.kartenOrakelImage);
 		progress = (ProgressBar) findViewById(R.id.progressBarKarten);
 		istNeuesSpiel = true;
-		
+
 		//onclick listener für Kartenstapel
 		image.setOnClickListener(new View.OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
@@ -46,89 +47,96 @@ public class KartenOrakloid extends Activity {
 					String symbol = orakel.kartenSymbolBestimmen(karte[0]);
 					int wert = orakel.kartenWertBestimmen(karte[0]);
 					orakel.paint(orakel, image, symbol, wert);
-					ausgabe.setText(orakel.spieleKartenOrakel(karte[0], true)); //bool hier noch unrelevant
-					
+					ausgabe.setText(orakel.spieleKartenOrakel(karte[0], true)); // bool hier noch unrelevant
 					hoeherButton.setVisibility(0);
 					tieferButton.setVisibility(0);
-					
 					progress.incrementProgressBy(1);
 					istNeuesSpiel = false;
-				}
-				else {
-					//deaktiviert wenn das Spiel läuft
+				} else {
+					// onClick macht nichts mehr sobald das Spiel läuft
 				}
 			}
 		});
-		
-		//onclick listener für tiefer Button
+
+		// onclick listener für tiefer Button
 		tieferButton.setOnClickListener(new View.OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				karte = orakel.kartenZiehen(1);
-				
-				if (karte[0].equals("Deck enthält zu wenig Karten")) {
-					ausgabe.setText("Alle Karten durch, klicken sie auf den Stapel " +
-							"um neu zu starten");
-					istNeuesSpiel = true;
-					image.setImageResource(R.drawable.deckblatt);
-					hoeherButton.setVisibility(4);
-					tieferButton.setVisibility(4);
-					progress.setProgress(0);
-					
-					orakel = new KartenOrakel();
-					richtigLabel.setText("Richtige: 0");
-					falschLabel.setText("Falsche: 0");
-				}
-				else {
-					karte = orakel.kartenZiehen(1);
-					String symbol = orakel.kartenSymbolBestimmen(karte[0]);
-					int wert = orakel.kartenWertBestimmen(karte[0]);
-					orakel.paint(orakel, image, symbol, wert);
-					ausgabe.setText(orakel.spieleKartenOrakel(karte[0], false));
-	
-					richtigLabel.setText("Richtige: " +orakel.getAnzahlRichtigeTreffer());
-					falschLabel.setText("Falsche: " +orakel.getAnzahlFalscheTreffer());
-					
-					progress.incrementProgressBy(1);
-				}
+				if (karte[0].equals("Deck enthält zu wenig Karten"))
+					reset();
+				else
+					zugMachen(false);
 			}
 		});
-		
-		//onclick listener für höher Button
+
+		// onclick listener für höher Button
 		hoeherButton.setOnClickListener(new View.OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				karte = orakel.kartenZiehen(1);
-				
-				if (karte[0].equals("Deck enthält zu wenig Karten")) {
-					ausgabe.setText("Alle Karten durch, klicken sie auf den Stapel" +
-							"um neu zu starten");
-					istNeuesSpiel = true;
-					image.setImageResource(R.drawable.deckblatt);
-					hoeherButton.setVisibility(4);
-					tieferButton.setVisibility(4);
-					progress.setProgress(0);
-					
-					orakel = new KartenOrakel();
-					richtigLabel.setText("Richtige: 0");
-					falschLabel.setText("Falsche: 0");
-				}
-				else {
-					String symbol = orakel.kartenSymbolBestimmen(karte[0]);
-					int wert = orakel.kartenWertBestimmen(karte[0]);
-					orakel.paint(orakel, image, symbol, wert);
-					ausgabe.setText(orakel.spieleKartenOrakel(karte[0], true));
-					
-					richtigLabel.setText("Richtige: " +orakel.getAnzahlRichtigeTreffer());
-					falschLabel.setText("Falsche: " +orakel.getAnzahlFalscheTreffer());
-					
-					progress.incrementProgressBy(1);
-				}
+				if (karte[0].equals("Deck enthält zu wenig Karten"))
+					reset();
+				else
+					zugMachen(true);
 			}
 		});
+	}
+
+	private void zugMachen(boolean hoeherButtonGedrückt) {
+		String symbol = orakel.kartenSymbolBestimmen(karte[0]);
+		int wert = orakel.kartenWertBestimmen(karte[0]);
+		orakel.paint(orakel, image, symbol, wert);
+		ausgabe.setText(orakel.spieleKartenOrakel(karte[0], hoeherButtonGedrückt));
+
+		richtigLabel.setText("Richtige: " + orakel.getAnzahlRichtigeTreffer());
+		falschLabel.setText("Falsche: " + orakel.getAnzahlFalscheTreffer());
+
+		progress.incrementProgressBy(1);
+	}
+
+	private void reset() {
+		resultatAnzeigen();
+		ausgabe.setText("Alle Karten durch, klicken sie auf den Stapel"
+				+ "um neu zu starten");
+		istNeuesSpiel = true;
+		image.setImageResource(R.drawable.deckblatt);
+		hoeherButton.setVisibility(4);
+		tieferButton.setVisibility(4);
+		progress.setProgress(0);
+
+		orakel = new KartenOrakel();
+		richtigLabel.setText("Richtige: 0");
+		falschLabel.setText("Falsche: 0");
+	}
+	
+	private void resultatAnzeigen() {
+		AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+		dialog.setTitle("Resultat");
+		String message;
+		if (orakel.getAnzahlRichtigeTreffer() > 35) {
+			message =
+					"\nRichtig phrophezeit: " + orakel.getAnzahlRichtigeTreffer() + " mal\n" +
+					"\nFalsch prophezeit: " + orakel.getAnzahlFalscheTreffer() + " mal\n" +
+					"Titel: Gott";
+		}
+		else if (orakel.getAnzahlRichtigeTreffer() > 26) {
+			message =
+					"\nRichtig phrophezeit: " + orakel.getAnzahlRichtigeTreffer() + " mal\n" +
+					"\nFalsch prophezeit: " + orakel.getAnzahlFalscheTreffer() + " mal\n" +
+					"Titel: Passables Orakel";
+		}
+		else {
+			message = 
+					"\nRichtig phrophezeit: " + orakel.getAnzahlRichtigeTreffer() + " mal\n" +
+					"\nFalsch prophezeit: " + orakel.getAnzahlFalscheTreffer() + " mal\n" +
+					"Titel: Scharlatan";
+		}
+		dialog.setMessage(message);
+		dialog.show();
 	}
 }
