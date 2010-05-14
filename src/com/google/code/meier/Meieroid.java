4f1d46cad4c559462a4748ddb9a4a11c224338d3
Rehgon <Rehgon@5ce971c1-67d6-bedd-deea-c9cf13bb2f1f>
2010-05-14 01:54:35 +0000

    Remo Blättler 14.05.10 03.51:
    	- Meier
    		- Implementiert
    		- Noch in Arbeit

---
+++ src/com/google/code/meier/Meieroid.java
@@ -0,0 +1,43 @@
+package com.google.code.meier;
+
+import com.google.code.trinkspiele.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class Meieroid extends Activity {
+
+	ImageView image, wuerfelEins, wuerfelZwei;
+	boolean wuerfelSichtbar;
+
+	public void onCreate(Bundle savedInstanceState) {
+		
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.meier);
+		
+		wuerfelSichtbar = true;
+		
+		image = (ImageView) findViewById(R.id.meierImage);
+		wuerfelEins = (ImageView) findViewById(R.id.wuerfelEinsImage);
+		wuerfelZwei = (ImageView) findViewById(R.id.wuerfelZweiImage);
+		image.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if (wuerfelSichtbar) {
+					image.setImageResource(R.drawable.wuerfelbecher_gross);
+					wuerfelEins.setVisibility(4);
+					wuerfelZwei.setVisibility(4);
+				}
+				else {
+					image.setImageResource(R.drawable.wuerfelbecher_klein);
+					wuerfelEins.setVisibility(0);
+					wuerfelZwei.setVisibility(0);
+				}
+			}
+		});
+	}
+}
